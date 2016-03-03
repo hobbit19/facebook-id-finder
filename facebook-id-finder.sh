46d734807fdac5b560ae436daa98476556eb45d4
gyzheng <gyzheng@cs.nctu.edu.tw>
2016-03-03 17:31:40 +0800

    Fix output

--- facebook-id-finder.sh
+++ facebook-id-finder.sh
@@ -3,15 +3,15 @@
 # Mail: gyzheng@cs.nctu.edu.tw
 # Usage: ./facebook-id-finder.sh <url or username>
 
-echo "========== FB name2id Tool =========="
+echo "========== Facebook ID Finder =========="
 url="https://www.facebook.com/"
-echo "Step1 Parse your username..."
+echo "Step1: Parse the name..."
 username=`echo $1 | cut -d '/' -f 4`
 echo "===> "$username
-echo "Step2 Find your id..."
+echo "Step2: Find the id..."
 raw=`curl $url'/'$username 2>/dev/null | sed -e 's/}/\n/g; s/\"//g' | grep 'entity_id:[0-9]*' | cut -d ':' -f 3`
 if [ -z "$raw" ]; then
-    echo "Wrong username! Please re-try!"
+    echo "Wrong input name! Please re-try!"
 else
     echo "===> Finish"
     echo $username"<=>"$raw
